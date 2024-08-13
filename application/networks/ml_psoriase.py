83040220d2ce122c314437f3ae5d87156cb0b9a6
checkioname <lucasgargalhone@gmail.com>
2024-08-13 20:24:33 -0300

    refactor: separando responsabilidades entre classes

--- application/networks/ml_psoriase.py
+++ application/networks/ml_psoriase.py
@@ -2,24 +2,19 @@
 # coding: utf-8
 
 
-
-import numpy as np
 from random import randint
 import torch
-import torchvision
-import numpy as np
-import pandas as pd
 from torchvision.io import read_image
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import torch.nn as nn
-from PIL import Image
 import os
 from torchvision import datasets,transforms
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim.lr_scheduler import StepLR
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-from sklearn.model_selection import StratifiedKFold
+#from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+from application.networks.custom_network import NeuralNetwork
+from application.utils.training import Training
 
 #Pytorch possibilida o usa facil de gpu
 device = (
@@ -29,75 +24,6 @@ device = (
     if torch.backends.mps.is_available()
     else "cpu"
 )
-#print(f"Using {device} device")
-
-
-
-
-#transformando as imagens (image augmentation)
-#Normalize = normalifor image_file in os.listdir(dir):ze a tensor image with mean and standard deviation.
-#Pode transformar em grayscale? (acho que nao kkkkk)
-## Pre processamento
-
-transforms = transforms.Compose([
-    #transforms.RandomRotation(50,fill=1),
-    #transforms.RandomCrop(size=(224, 224)),
-    #transforms.RandomResizedCrop((224,224)),
-    transforms.Resize((224,224)),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomVerticalFlip(p=0.5),
-    transforms.ToTensor(),  # Converte para tensor
-])
-
-
-
-
-#Creating a custom dataset (rotulando, etc...)
-from torch.utils.data import Dataset
-from PIL import Image
-
-class CustomDataset(Dataset):
-    def __init__(self, csv_file, img_dir, transform=None, target_transform=None):
-        self.data = pd.read_csv(csv_file)
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        image_name = self.data.loc[idx, 'img_name']
-        image_path = os.path.join(self.img_dir, image_name)
-        image = Image.open(image_path)
-        label = str(self.data.loc[idx, 'labels'])
-        
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.transform(label)
-        
-        return image, label
-
-# Criando KFold cross-validator
-kf = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=42)
-
-# Criando DataLoader para o conjunto de treinamento
-custom_dataset = CustomDataset(csv_file='data_labels.csv', img_dir='/content/sample_data/psoriasis/data', transform=transforms, target_transform=None)
-
-labels = custom_dataset.labels
-for fold, (train_index, val_index) in enumerate(kf.split(range(len(labels)), labels)):
-    print(f"Fold {fold + 1}/{num_folds}")
-
-    # Salvando os índices de treinamento e validação
-    torch.save(train_index, os.path.join(f'/content/drive/MyDrive/psoriasis/train_index_fold{fold}.pt'))
-    torch.save(val_index, os.path.join(f'/content/drive/MyDrive/psoriasis/val_index_fold{fold}.pt'))
-
-    print(train_index, val_index)
-
-
-
-from torchvision.transforms import ToTensor
 
 train_dataset = CustomDataset(csv_file='train_set.csv', img_dir='data/', transform=transforms,target_transform=None )
 test_dataset = CustomDataset(csv_file='test_set.csv',img_dir='data/', transform=transforms)
@@ -153,71 +79,7 @@ tests = [test_6,
          test_8,
          test_9,
          test_10,
-         test_11]         
-
-
-
-
-
-# Defina uma semente para a inicialização
-seed = 422
-torch.manual_seed(seed)
-
-class Flatten(nn.Module):
-    def forward(self, x):
-        return x.view(x.size(0), -1)
-
-# O resize da nossa imagem foi para 224x224 e ela é colorida ( 3 channels)
-class NeuralNetwork(nn.Module):
-    def __init__(self, test, dropout_prob=0.5):
-        super(NeuralNetwork, self).__init__()
-        
-        # Tamanho da imagem de entrada
-        self.input_size = (3, 224, 224)
-        
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(3, test[0], kernel_size=3, padding=0),
-            nn.SELU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(test[0], test[1], kernel_size=3, padding=0),
-            nn.SELU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(test[1], test[2], kernel_size=3, padding=0),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(test[2], test[3], kernel_size=3, padding=0),
-            nn.SELU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(test[3], test[4], kernel_size=3, padding=0),
-            nn.SELU(),
-            nn.MaxPool2d(kernel_size=2, stride=1)
-        )
-
-        self.dropout = nn.Dropout(p=dropout_prob)
-
-        # Calcule o tamanho da camada linear de acordo com o tamanho de entrada
-        self.flatten = Flatten()
-        flattened_size = self._get_flattened_size(self.input_size)
-
-        self.fc_layers = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(flattened_size, int(flattened_size/2)),
-            nn.SELU(),
-            nn.Linear(int(flattened_size/2), 1),
-            nn.Sigmoid()
-        )
-        
-    def forward(self, x):
-        x = self.conv_layers(x)
-        x = self.dropout(x)
-        x = self.fc_layers(x)
-        return x
-
-    def _get_flattened_size(self, input_size):
-        with torch.no_grad():
-            dummy_input = torch.zeros(1, *input_size)
-            output = self.conv_layers(dummy_input)
-            flattened_output = self.flatten(output)
-            return flattened_output.size(1)
+         test_11]
 
 
 #batch_labels_numeric = [class_to_idx[label] for label in batch_labels]
@@ -238,52 +100,12 @@ def testing_entries(model,dataloader):
         break
 
 
+training = Training()
 
-# Função de treinamento
-def train(dataloader, model, loss_fn, optimizer, class_to_idx, writer, device,epoch):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        batch_labels_numeric = [class_to_idx[label] for label in y]
-        y = torch.tensor(batch_labels_numeric, dtype=torch.float32).to(device)
-        X, y = X.to(device), y.to(device)
+training.test()
 
-        optimizer.zero_grad()
+training.train()
 
-        pred = model(X)
-        pred = pred.squeeze(1)
-
-        loss = loss_fn(pred, y)
-        writer.add_scalar("Loss/train", loss, epoch)
-        loss.backward()
-        optimizer.step()
-
-        if batch % 100 == 0:
-            current = batch * len(X)
-            print(f"Loss: {loss.item():.7f}  [{current:>5d}/{size:>5d}]")
-
-# Função de teste
-def test(data, model, loss_fn, class_to_idx, epoch, writer, device):
-    size = len(data.dataset)
-    num_batches = len(data)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in data:
-            batch_labels_numeric = [class_to_idx[label] for label in y]
-            y = torch.tensor(batch_labels_numeric, dtype=torch.float32).to(device)
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            pred = pred.squeeze(1)
-            test_loss += loss_fn(pred, y).item()
-            correct += (torch.round(pred) == y).sum().item()
-    
-    test_loss /= num_batches
-    correct /= size
-    accuracy = 100 * correct
-    writer.add_scalar("Loss/test", test_loss, epoch)
-    writer.add_scalar("Accuracy/test", accuracy, epoch)
-    print(f"Test Error: Accuracy: {accuracy:.2f}% | Avg loss: {test_loss:.8f}\n")
 
 # Exemplo de uso
 class_to_idx = {"psoriasis": 1, "melanome": 0}
