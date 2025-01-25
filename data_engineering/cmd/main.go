8fd37f520b408c769a9b6e2e928cb39a520fe0ec
checkioname <lucasgargalhone@gmail.com>
2025-01-25 18:36:41 -0300

    script para gerar csv do dataset funcionando

--- data_engineering/cmd/main.go
+++ data_engineering/cmd/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"flag"
+	"os"
+	"strings"
+
+	e "github.com/checkioname/cnn-skin-model/internal/utils"
+)
+
 func main() {
+
+  // Gerar Dataset
+  rootpath := flag.String("p", "/home/king/Documents/PsoriasisEngineering/cnn-skin-model/model_engineering/infrastructure/db/", "caminho para gerar o dataset")
+  gendataset := flag.Bool("d", false, "Gerar ou nao o dataset")
+
+
+  // Parse flags
+  flag.Parse()
+  
+
+
+
+  // Handling flags
+  if (*gendataset == true) {
+    
+    wd, _ := os.Getwd()
+    baseDir := strings.Join(strings.Split(wd, "/")[:len(strings.Split(wd, "/")) -1],"/") 
+
+    filename := baseDir + "/model_engineering/dataset.csv"
+    e.GenerateCsvFromDir(*rootpath, filename)
+    println(filename)
+
+  }
 
 }
